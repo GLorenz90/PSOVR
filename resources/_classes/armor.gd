7b903ee3e7d18d86e32a07502d697b9f72f71898
Grayson Lorenz <graysonc.l.lorenz@gmail.com>
2025-04-05 13:50:10 -0500

    updates

--- resources/_classes/armor.gd
+++ resources/_classes/armor.gd
@@ -1,7 +1,7 @@
 extends Equipment
 class_name Armor
 
-const MAX_SLOTS := 3;
+const MAX_SLOTS := 4;
 
 @export var ARMOR_TYPE: Enums.ARMOR_TYPES = Enums.ARMOR_TYPES.LIGHT_ARMOR;
 @export_range(0, MAX_SLOTS) var SLOTS := 0;
