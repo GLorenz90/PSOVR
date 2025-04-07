81c48e3dedf390b965640c8fddeb318b5a7f3e51
Grayson Lorenz <graysonc.l.lorenz@gmail.com>
2025-04-07 13:38:57 -0500

    adding script for player class

---
+++ scenes/player/xr_origin.gd
@@ -0,0 +1,27 @@
+extends XROrigin3D
+
+func _on_left_hand_button_pressed(name: String) -> void:
+  print(name)
+  pass # Replace with function body.
+
+func _on_right_hand_button_pressed(name: String) -> void:
+  print(name)
+  pass # Replace with function body.
+  
+func _on_left_hand_button_released(name: String) -> void:
+  print(name)
+  pass # Replace with function body.
+  
+func _on_right_hand_button_released(name: String) -> void:
+  print(name)
+  pass # Replace with function body.
+
+#region FUNCTIONS ==================================================================================
+func openHandQuickMenu(hand: XRController3D):
+  
+  pass
+
+func closeHandQuickMenu(hand: XRController3D):
+  
+  pass
+#endregion
